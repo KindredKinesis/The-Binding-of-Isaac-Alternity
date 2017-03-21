0f6e5a9937a915cde2fa94f0bf941862cae28499
Katie Schembri <ethanthelion@gmail.com>
2017-03-21 20:13:12 +1100

    Code and XML stuff for Cloak and Dagger (Very WIP)
    This is a very WIP version of Cloak and Dagger which spawns a dagger
    (Textured as Mom's Knife for now) if you have been shooting fo 2 seconds
    that spins around the player dealing 20 damage.

--- main.lua
+++ main.lua
@@ -8,6 +8,8 @@ local DebugText = {}; --Not used in release
 -----VARIABLES-----
 
 local ExcaliburItem = Isaac.GetItemIdByName("Excalibur"); --Gets the item ID of Excalibur
+local CloakAndDaggerItem; --THIS NEEDS TO BE FILLED OUT
+local CADKnife = Isaac.GetEntityVariantByName("CloakDagger"); --Gets the variant of CloakDagger (Knife for Cloak and Dagger)
 
 -----FUNCTIONS-----
 
@@ -32,9 +34,66 @@ function Alternity:UseExcalibur()
   return true --Makes the player hold the item above their head
 end
 
+function Alternity:CloakAndDaggerEffect()
+  local player = Isaac.GetPlayer(0) --Gets the player
+  local entities = Isaac.GetRoomEntities() --Gets every entity in the room
+  local playerdata = player:GetData() --Gets the player's data
+  
+  if playerdata.CloakAndDagger == nil or player:GetFireDirection() == Direction.NO_DIRECTION then --If the player doesn't have a Cloak and Dagger charge or if they aren't shooting
+    playerdata.CloakAndDagger = 0 --Set the Cloak and Dagger charge to 0
+  else
+    playerdata.CloakAndDagger = playerdata.CloakAndDagger + 1 --Increase Cloak and Dagger charge by 1
+  end
+  
+  if playerdata.CloakAndDagger == 60 then --If Cloak and Dagger charge equals 60 (2 seconds or so)
+    local knife = Isaac.Spawn(3,CADKnife,0,player.Position,Vector(0,0),player)
+    local data = knife:GetData()
+    data.MoveX = 0
+    data.MoveY = 0
+    data.Radius = 30
+    data.XSign = 1
+    data.YSign = 1
+    knife.CollisionDamage = 20
+    playerdata.CloakAndDagger = 0
+  end
+end
+
+function Alternity:CADKnifeUpdate(knife)
+  local player = Isaac.GetPlayer(0)
+  local sprite = knife:GetSprite()
+  local data = knife:GetData()
+  
+  DebugText = {}
+  table.insert(DebugText,data.MoveX)
+  table.insert(DebugText,data.MoveY)
+  table.insert(DebugText,data.XSign)
+  table.insert(DebugText,data.YSign)
+  table.insert(DebugText,data.Radius)
+  
+  if data.MoveX >= data.Radius then
+    data.XSign = -1
+    data.YSign = -1
+  elseif data.MoveX <= (data.Radius * -1) then
+    data.XSign = 1
+    data.YSign = 1
+  end
+  
+  data.MoveX = data.MoveX + (data.XSign * 5)
+  data.MoveY = math.floor(math.sqrt(data.Radius^2 - data.MoveX^2) * data.YSign)
+  
+  knife.Position = Vector(player.Position.X + data.MoveX, player.Position.Y - 10 + data.MoveY)
+  sprite.Rotation = sprite.Rotation + 60
+  
+  --if knife.FrameCount == 120 then
+    --knife:Remove()
+  --end
+end
+
 -----CALLBACKS-----
 
 Alternity:AddCallback(ModCallbacks.MC_USE_ITEM, Alternity.UseExcalibur, ExcaliburItem) --Use item callback, UseExcalibur function, Excalibur item
+Alternity:AddCallback(ModCallbacks.MC_POST_UPDATE, Alternity.CloakAndDaggerEffect) --Post update callback, CloakAndDaggerEffect function
+Alternity:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, Alternity.CADKnifeUpdate, CADKnife) --Familiar update callback, CADKnifeUpdate function, CADKnife familiar
 
 
 
