23c925052043c7bf8c03a098f426a9beb8d76a0e
Katie Schembri <ethanthelion@gmail.com>
2017-04-17 13:19:44 +1000

    Code for Alpha Crest
    There's currently debug text showing in this code, it can be disabled by
    removing the callback at the bottom for the Alternity.Debug function.

--- main.lua
+++ main.lua
@@ -12,6 +12,9 @@ local CloakAndDaggerItem = Isaac.GetItemIdByName("Cloak and Dagger")
 local CloakDaggerVariant = Isaac.GetEntityVariantByName("CloakDagger")
 local CloakAndDaggerInvis = false
 
+local AlphaCrestItem = Isaac.GetItemIdByName("Alpha Crest")
+local AlphaCrestActive = false
+
 ---<<FUNCTIONS>>---
 
 ----<EXCALIBUR>----
@@ -93,6 +96,72 @@ function Alternity:CloakAndDaggerDamage(Ent,DamageAmount,_,DamageSource,_)
   end
 end
 
+----<ALPHA CREST>----
+function Alternity:AlphaCrestEffect(Ent,DamageAmount,_,DamageSource,_)
+  local player = Isaac.GetPlayer(0)
+  local entities = Isaac.GetRoomEntities()
+  
+  if player:HasCollectible(AlphaCrestItem) and Ent:IsEnemy() and Ent.Type ~= EntityType.ENTITY_FIREPLACE then
+    if AlphaCrestActive then
+      Ent.HitPoints = Ent.HitPoints - (DamageAmount * 2)
+      return true
+    end
+  end
+end
+
+local DebugText = {}
+
+function Alternity:AlphaCrestActivate()
+  local player = Isaac.GetPlayer(0)
+  local entities = Isaac.GetRoomEntities()
+  local room = Game():GetLevel():GetCurrentRoom()
+  local data = player:GetData()
+  
+  if player:HasCollectible(AlphaCrestItem) then
+    if room:GetFrameCount() == 1 then
+      data.AlphaCount = 0
+    end
+    
+    local totalenemies = 0
+    
+    for i = 1, #entities do
+      if entities[i]:IsEnemy() and entities[i].Type ~= EntityType.ENTITY_FIREPLACE then
+        totalenemies = totalenemies + 1
+      end
+    end
+    
+    if totalenemies > data.AlphaCount then
+      data.AlphaCount = totalenemies
+    end
+    
+    local currenemies = 0
+    
+    for i = 1, #entities do
+      if entities[i]:IsEnemy() and entities[i].Type ~= EntityType.ENTITY_FIREPLACE then
+        currenemies = currenemies + 1
+      end
+    end
+    
+    if currenemies >= (data.AlphaCount * 0.66) and data.AlphaCount > 3 then
+      AlphaCrestActive = true
+    else
+      AlphaCrestActive = false
+    end
+    
+    DebugText[1] = "AlphaCount: " .. data.AlphaCount
+    DebugText[2] = "Enemies: " .. currenemies
+    DebugText[3] = "AlphaCount * 0.66: " .. (data.AlphaCount * 0.66)
+    DebugText[4] = "TotalEnemies: " .. totalenemies
+    DebugText[5] = "AlphaCrestActive: " .. tostring(AlphaCrestActive)
+  end
+end
+
+function Alternity:Debug()
+  for i = 1, #DebugText do
+    Isaac.RenderText(tostring(DebugText[i]),50,30 + (20*i),255,0,0,255)
+  end
+end
+
 ---<<CALLBACKS>>---
 
 Alternity:AddCallback(ModCallbacks.MC_USE_ITEM, Alternity.UseExcalibur, ExcaliburItem)
@@ -100,3 +169,8 @@ Alternity:AddCallback(ModCallbacks.MC_USE_ITEM, Alternity.UseExcalibur, Excalibu
 Alternity:AddCallback(ModCallbacks.MC_POST_UPDATE,Alternity.CloakAndDaggerEffect)
 Alternity:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE,Alternity.CloakAndDaggerUpdate,CloakDaggerVariant)
 Alternity:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG,Alternity.CloakAndDaggerDamage)
+
+Alternity:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG,Alternity.AlphaCrestEffect)
+Alternity:AddCallback(ModCallbacks.MC_POST_UPDATE,Alternity.AlphaCrestActivate)
+
+Alternity:AddCallback(ModCallbacks.MC_POST_RENDER,Alternity.Debug)
