6f5548fd44da017221d1bc7519bbd1007f7aca77
Katie Schembri <ethanthelion@gmail.com>
2017-05-05 17:06:47 +1000

    Framework and Golden Fleece
    I implemented some framework that is subject to change. I also merged
    Golden Fleece into the mod.

--- main.lua
+++ main.lua
@@ -1,29 +1,57 @@
 local Alternity = RegisterMod("Alternity",1)
 
----------------
---<<<ITEMS>>>--
----------------
+-------------------
+--<<<FRAMEWORK>>>--
+-------------------
+
+---<<TABLES>>---
+Alternity.Items = {
+  Passives = {
+    CLOAK_AND_DAGGER = Isaac.GetItemIdByName("Cloak and Dagger"),
+    ALPHA_CREST = Isaac.GetItemIdByName("Alpha Crest"),
+    GOLDEN_FLEECE = Isaac.GetItemIdByName("Golden Fleece")
+  },
+  Actives = {
+    EXCALIBUR = Isaac.GetItemIdByName("Excalibur")
+  },
+  Familiars = {
+  },
+  Trinkets = {
+  }
+}
+
+Alternity.ItemVariables = {
+  CloakAndDagger = {
+      Variant = Isaac.GetEntityVariantByName("CloakDagger"),
+      Invisible = false
+  },
+  AlphaCrest = {
+    Active = false
+  },
+  GoldenFleece = {
+    invulnerabilityTimeOut = 0
+  }
+}
 
 ---<<VARIABLES>>---
+local Passive = Alternity.Items.Passives
+local Active = Alternity.Items.Actives
+local Familiar = Alternity.Items.Familiars
+local Trinket = Alternity.Items.Trinkets
 
-local ExcaliburItem = Isaac.GetItemIdByName("Excalibur")
+local ItemVars = Alternity.ItemVariables
 
-local CloakAndDaggerItem = Isaac.GetItemIdByName("Cloak and Dagger")
-local CloakDaggerVariant = Isaac.GetEntityVariantByName("CloakDagger")
-local CloakAndDaggerInvis = false
+-----------------
+--<<<ACTIVES>>>--
+-----------------
 
-local AlphaCrestItem = Isaac.GetItemIdByName("Alpha Crest")
-local AlphaCrestActive = false
-
----<<FUNCTIONS>>---
-
-----<EXCALIBUR>----
+---<<EXCALIBUR>>---
 function Alternity:UseExcalibur()
   local player = Isaac.GetPlayer(0)
   
   if player:GetMaxHearts() > 2 then
     if player:GetMaxHearts() == 4 then
-      player:AddCollectible(CloakAndDaggerItem,0,true)
+      player:AddCollectible(Passive.CLOAK_AND_DAGGER,0,true)
     elseif player:GetMaxHearts() == 6 then
       player:AddCollectible(CollectibleType.COLLECTIBLE_SPEAR_OF_DESTINY,0,true)
     elseif player:GetMaxHearts() == 8 then
@@ -32,7 +60,7 @@ function Alternity:UseExcalibur()
       player:AddCollectible(CollectibleType.COLLECTIBLE_MOMS_KNIFE,0,true)
     end
     
-    player:RemoveCollectible(ExcaliburItem)
+    player:RemoveCollectible(Active.EXCALIBUR)
     
     return true
   else
@@ -40,13 +68,19 @@ function Alternity:UseExcalibur()
   end
 end
 
-----<CLOAK AND DAGGER>----
+Alternity:AddCallback(ModCallbacks.MC_USE_ITEM, Alternity.UseExcalibur, Active.EXCALIBUR)
+
+------------------
+--<<<PASSIVES>>>--
+------------------
+
+---<<CLOAK AND DAGGER>>---
 function Alternity:CloakAndDaggerEffect()
   local player = Isaac.GetPlayer(0)
   local entities = Isaac.GetRoomEntities()
   local playerdata = player:GetData()
   
-  if player:HasCollectible(CloakAndDaggerItem) then
+  if player:HasCollectible(Passive.CLOAK_AND_DAGGER) then
     if playerdata.CloakAndDagger == nil or player:GetFireDirection() == Direction.NO_DIRECTION then
       playerdata.CloakAndDagger = 0
     else
@@ -57,21 +91,23 @@ function Alternity:CloakAndDaggerEffect()
       playerdata.InvisTimeout = 120
     elseif playerdata.InvisTimeout <= 0 then
       playerdata.InvisTimeout = 120
-      CloakAndDaggerInvis = false
+      ItemVars.CloakAndDagger.Invisible = false
     end
     
-    if CloakAndDaggerInvis == true then
+    if ItemVars.CloakAndDagger.Invisible then
       player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_CAMO_UNDIES,false)
       playerdata.InvisTimeout = playerdata.InvisTimeout - 1
     end
     
     if playerdata.CloakAndDagger == 150 then
-      local knife = Isaac.Spawn(3,CloakDaggerVariant,0,player.Position,Vector(0,0),player)
+      local knife = Isaac.Spawn(EntityType.ENTITY_FAMILIAR,ItemVars.CloakAndDagger.Variant,0,player.Position,Vector(0,0),player)
       knife.CollisionDamage = 10
       playerdata.CloakAndDagger = 0
     end
   end
 end
+
+Alternity:AddCallback(ModCallbacks.MC_POST_UPDATE,Alternity.CloakAndDaggerEffect)
 
 function Alternity:CloakAndDaggerUpdate(knife)
   local player = Isaac.GetPlayer(0)
@@ -84,32 +120,37 @@ function Alternity:CloakAndDaggerUpdate(knife)
   end
 end
 
-function Alternity:CloakAndDaggerDamage(Ent,DamageAmount,_,DamageSource,_)
+Alternity:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE,Alternity.CloakAndDaggerUpdate,ItemVars.CloakAndDagger.Variant)
+
+function Alternity:CloakAndDaggerDamage(Ent,DamageAmount,DamageFlags,DamageSource,CountdownFrames)
   local player = Isaac.GetPlayer(0)
   
-  if player:HasCollectible(CloakAndDaggerItem) then
-    if DamageSource.Type == 3 and DamageSource.Variant == CloakDaggerVariant then
+  if player:HasCollectible(Passive.CLOAK_AND_DAGGER) then
+    if DamageSource.Type == EntityType.ENTITY_FAMILIAR and DamageSource.Variant == ItemVars.CloakAndDagger.Variant then
       if DamageAmount >= Ent.HitPoints then
-        CloakAndDaggerInvis = true
+        ItemVars.CloakAndDagger.Invisible = true
       end
     end
   end
 end
 
-----<ALPHA CREST>----
+Alternity:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG,Alternity.CloakAndDaggerDamage)
+
+
+---<<ALPHA CREST>>---
 function Alternity:AlphaCrestEffect(Ent,DamageAmount,_,DamageSource,_)
   local player = Isaac.GetPlayer(0)
   local entities = Isaac.GetRoomEntities()
   
-  if player:HasCollectible(AlphaCrestItem) and Ent:IsActiveEnemy(false) then
-    if AlphaCrestActive then
+  if player:HasCollectible(Passive.ALPHA_CREST) and Ent:IsActiveEnemy(false) then
+    if ItemVars.AlphaCrest.Active then
       Ent.HitPoints = Ent.HitPoints - (DamageAmount * 2)
       return true
     end
   end
 end
 
-local DebugText = {}
+Alternity:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG,Alternity.AlphaCrestEffect)
 
 function Alternity:AlphaCrestActivate()
   local player = Isaac.GetPlayer(0)
@@ -117,7 +158,7 @@ function Alternity:AlphaCrestActivate()
   local room = Game():GetLevel():GetCurrentRoom()
   local data = player:GetData()
   
-  if player:HasCollectible(AlphaCrestItem) then
+  if player:HasCollectible(Passive.ALPHA_CREST) then
     if room:GetFrameCount() == 1 then
       data.AlphaCount = 0
     end
@@ -137,40 +178,43 @@ function Alternity:AlphaCrestActivate()
     local currenemies = 0
     
     for i = 1, #entities do
-      if entities[i]:IsActiveEnemy() then
+      if entities[i]:IsActiveEnemy(false) then
         currenemies = currenemies + 1
       end
     end
     
     if currenemies >= (data.AlphaCount * 0.66) and data.AlphaCount > 3 then
-      AlphaCrestActive = true
+      ItemVars.AlphaCrest.Active = true
     else
-      AlphaCrestActive = false
+      ItemVars.AlphaCrest.Active = false
     end
-    
-    DebugText[1] = "AlphaCount: " .. data.AlphaCount
-    DebugText[2] = "Enemies: " .. currenemies
-    DebugText[3] = "AlphaCount * 0.66: " .. (data.AlphaCount * 0.66)
-    DebugText[4] = "TotalEnemies: " .. totalenemies
-    DebugText[5] = "AlphaCrestActive: " .. tostring(AlphaCrestActive)
   end
 end
 
-function Alternity:Debug()
-  for i = 1, #DebugText do
-    Isaac.RenderText(tostring(DebugText[i]),50,30 + (20*i),255,0,0,255)
-  end
-end
-
----<<CALLBACKS>>---
-
-Alternity:AddCallback(ModCallbacks.MC_USE_ITEM, Alternity.UseExcalibur, ExcaliburItem)
-
-Alternity:AddCallback(ModCallbacks.MC_POST_UPDATE,Alternity.CloakAndDaggerEffect)
-Alternity:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE,Alternity.CloakAndDaggerUpdate,CloakDaggerVariant)
-Alternity:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG,Alternity.CloakAndDaggerDamage)
-
-Alternity:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG,Alternity.AlphaCrestEffect)
 Alternity:AddCallback(ModCallbacks.MC_POST_UPDATE,Alternity.AlphaCrestActivate)
 
-Alternity:AddCallback(ModCallbacks.MC_POST_RENDER,Alternity.Debug)
+
+---<<GOLDEN FLEECE>>---
+function Alternity:GoldenFleeceEffect(entity, amount, damageflag, source, countdownframes)
+  local player = Isaac.GetPlayer(0)
+  
+  if entity.Type == EntityType.ENTITY_PLAYER then
+    if player:HasCollectible(Passive.GOLDEN_FLEECE) then
+      chance = math.min(30, player:GetNumCoins()) * 0.87
+      chance = chance + math.max(0, player:GetNumCoins() - 30) * 0.1
+      
+      if math.random(1, 100) < chance then
+        ItemVars.GoldenFleece.invulnerabilityTimeOut = Game():GetFrameCount() + 6
+        player:SetColor(Color(1,1,0,1,0,0,0), 10, 1, true, false)
+      end
+      
+      if Game():GetFrameCount() <= ItemVars.GoldenFleece.invulnerabilityTimeOut then
+        return false
+      else
+        ItemVars.GoldenFleece.invulnerabilityTimeOut = -1
+      end
+    end
+  end
+end
+
+Alternity:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, Alternity.GoldenFleeceEffect)
